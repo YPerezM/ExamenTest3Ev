ec1af3fce7fcfb5a6a96c3233d1c43fb0d3c3892
yperezmartinez <yperezmartinez@atanasoft25.danielcastelao.org>
2015-06-02 10:32:34 +0200

    Commit Test Unitario obtenerMCD hecho

---
+++ test/mcd/TestParametrizados.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mcd;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author yperezmartinez
+ */
+@RunWith(Parameterized.class)
+public class TestParametrizados {
+    
+    
+}
