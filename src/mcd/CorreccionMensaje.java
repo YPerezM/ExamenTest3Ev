78a841962165c6b5d92d454336d9b7eac28fedfa
yperezmartinez <yperezmartinez@atanasoft25.danielcastelao.org>
2015-06-02 11:45:59 +0200

    Commit Empezando la clase nueva de la correccion del mensaje

---
+++ src/mcd/CorreccionMensaje.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mcd;
+
+/**
+ *
+ * @author yperezmartinez
+ */
+public class CorreccionMensaje {
+    
+     public static String visualizar(Calculo cal) {
+        String mens = "";
+        if (cal.getNumero1() < 0 && cal.getNumero2() < 0) {
+            mens = cal.getMensajeResultado();
+          System.out.println(mens);
+        
+    
+}
